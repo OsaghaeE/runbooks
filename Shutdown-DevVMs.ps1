8d72966dbbfa1487a0df9ba56751fe1825df5cbf
OsaghaeE <osaghaeedobor@hotmail.com>
2025-05-30 18:08:24 +1000

    Create Shutdown-DevVMs.ps1

---
+++ Shutdown-DevVMs.ps1
@@ -0,0 +1,24 @@
+workflow Shutdown-DevVMs {
+    param (
+        [string]$SubscriptionId = 'y42dff4cd-ba03-4206-94c5-721389b64c1e',
+        [string]$ResourceGroup = 'RG_Monitoring',
+        [string]$TagName = 'Environment',
+        [string]$TagValue = 'Dev'
+    )
+
+    Write-Output "Connecting to Azure..."
+    Connect-AzAccount -Identity
+
+    Write-Output "Setting subscription context..."
+    Set-AzContext -SubscriptionId $SubscriptionId
+
+    Write-Output "Getting VMs with tag $TagName=$TagValue..."
+    $vms = Get-AzVM -ResourceGroupName $ResourceGroup | Where-Object {
+        $_.Tags[$TagName] -eq $TagValue
+    }
+
+    foreach -parallel ($vm in $vms) {
+        Write-Output "Stopping VM: $($vm.Name)..."
+        Stop-AzVM -ResourceGroupName $ResourceGroup -Name $vm.Name -Force
+    }
+}
